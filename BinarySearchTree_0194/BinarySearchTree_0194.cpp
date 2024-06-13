6e18c7fae99093a6ef88a959a88a847814d5dfc7
syairaarif <putrisyafira4444@gmail.com>
2024-06-13 11:36:21 +0700

    membuat methode seach pada class binary tree

--- BinarySearchTree_0194/BinarySearchTree_0194.cpp
+++ BinarySearchTree_0194/BinarySearchTree_0194.cpp
@@ -9,7 +9,8 @@ public:
     string info;
     Node* leftchild;
     Node* rightchild;
-
+    
+    //constructor for the node class
     Node(string i, Node* l, Node* r)
     {
         info = i;
@@ -25,36 +26,54 @@ public:
 
     BinaryTree()
     {
-        ROOT = nullptr;
+        ROOT = nullptr;                         //intializing ROOT to null
     }
 
-    void insert(string element)
+    void insert(string element)                 //insert a node in the binary search tree
     {
-        Node* newNode = new Node(element, nullptr, nullptr);
-        newNode->info = element;
-        newNode->leftchild = nullptr;
-        newNode->rightchild = nullptr;
+        Node* newNode = new Node(element, nullptr, nullptr);        //allocate memory for the new node
+        newNode->info = element;                                    //assign value to the data flied of the new node
+        newNode->leftchild = nullptr;                               //make the left child of the new node point to null
+        newNode->rightchild = nullptr;                              //make the right child of the new node point to null
 
         Node* parent = nullptr;
         Node* currentNode = nullptr;
-        search(element, parent, currentNode);
+        search(element, parent, currentNode);                       //locate the node which will be the parent of the node to be ins
 
-        if(parent == nullptr)
+        if(parent == nullptr)                                       //if the parent is null(tree is empty)
         {
-            ROOT = newNode;
-            return;
+            ROOT = newNode;                                         //mark the new node as root
+            return;                                                 //exit
         }
-        if(element < parent->info)
+        if(element < parent->info)                                  //if the value in the data flied of the new node is less than that of the
         {
-            parent->leftchild = newNode;
+            parent->leftchild = newNode;                            //make the left child of the parent point to thd new node
         }
         else if(element > parent->info)
         {
             parent->rightchild = newNode;
         }
-
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        currentNode = ROOT;
+        parent = nullptr;
+
+        while((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
 };
+
+
+
 
 int main()
 {
